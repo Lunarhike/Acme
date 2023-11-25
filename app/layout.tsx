16041344fbff50716e917b1ce98012a4d7051bb8
lunarhike <lunarhike@gmail.com>
2023-11-25 11:37:42 +0100

    moved favicon, added metadata to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://acme-ruddy-seven.vercel.app/'),
+};
+
 export default function RootLayout({
   children,
 }: {
