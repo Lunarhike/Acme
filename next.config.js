839575ff38eb38a7640a1fcb476915e1f1f74014
lunarhike <lunarhike@gmail.com>
2024-01-18 18:41:03 +0100

    removed ppr and next canary

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // next-auth conflict - needs canary, but canary is not compatible yet
-  experimental: {
-    ppr: true,
-  },
 };
 
 module.exports = nextConfig;
